dc21758ec970dd2755bff333b8035d635c000bb9
pasosa-s <pablo91linkin@gmail.com>
2020-12-14 11:47:28 +0100

    [pablo] first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from 'react';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    console.log('todos', todoList);
+  }, [todoList]);
+
+  const handleInputChange = useCallback((e => {
+    setNewTodo(e.target.value);
+  }), []);
+
+  const handleSubmit = useCallback((e => {
+    e.preventDefault();
+    if(!newTodo.trim()) return;
+    setTodoList([
+      ...todoList, {
+        id: todoList.length + 1,
+        content: newTodo,
+        done: false
+      }
+    ])
+    setNewTodo("");
+  }), [newTodo, todoList]);
+
+  const handleCheckbox = useCallback((todo, index) => e => {
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1, {
+      ...todo,
+      done: !todo.done
+    })
+    setTodoList(newTodoList);
+  }, [todoList]);
+
+  const handleMarkAll = useCallback(() => {
+    const newTodoList = todoList.map(todo => {
+      todo.done = true;
+      return todo;
+    })
+    setTodoList(newTodoList);
+  }, [todoList]);
+
+  const handleRemoveTodo = useCallback((toRemove) => e => {
+    const newTodoList = todoList.filter(todo => todo !== toRemove);
+    setTodoList(newTodoList);
+  }, [todoList]);
+
+  const setColorRed = (todo) => {
+    const style = {backgroundColor: "#008744"}
+    if(todo.done) {
+      style.backgroundColor = "#d62d20"
+    }
+    return style
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">ToDo.</h1>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          className="input"
+          value={newTodo}
+          onChange={handleInputChange}
+        />
+        <button>Add TODO</button>
+      </form>
+      <button className="markall-button" onClick={handleMarkAll}>Mark all done</button>
+      <div className="todo-container">
+        {todoList.map((todo, index) => 
+        <div key={todo.id} className="todo-item" style={setColorRed(todo)}>
+          <input 
+            className="todo-checkbox" 
+            type="checkbox"
+            checked={todo.done}
+            onChange={handleCheckbox(todo, index)}
+          />
+         <span className={todo.done ? "done" : ""}>{todo.content}</span>
+          <button className="todo-button" onClick={handleRemoveTodo(todo)}>x</button>
+        </div>)}
+      </div>
     </div>
   );
 }
